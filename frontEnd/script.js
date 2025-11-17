7d5bf799862a65287e28b8db4f70a102b38a74f1
HANONA <HANONA@gmail.com>
2025-11-17 10:18:14 +0100

    changement dans file js

--- frontEnd/script.js
+++ frontEnd/script.js
@@ -1,12 +1,24 @@
-        function envoyer() {
-            let nom = document.getElementById("nom").value;
-            let email = document.getElementById("email").value;
-            let genre = document.getElementById("genre").value;
+function envoyer() {
+    let nom = document.getElementById("nom").value;
+    let email = document.getElementById("email").value;
+    let genre = document.getElementById("genre").value;
 
-            if (nom === "" || email === "" || genre === "") {
-                alert("Veuillez remplir tous les champs !");
-                return;
-            }
-
+    fetch("http://127.0.0.1:5000/submit", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            nom: nom,
+            email: email,
+            genre: genre
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.status === "success") {
             document.getElementById("successMsg").style.display = "block";
+        } else {
+            alert("Erreur : " + data.message);
         }
+    })
+    .catch(error => console.error(error));
+}
